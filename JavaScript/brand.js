572f3007ec9a8c9442e2da738c07e7c62c82b0f6
Alaa-AbuIssa <alaaabuissa95@gmail.com>
2021-03-21 00:55:08 +0200

    updeted

--- JavaScript/brand.js
+++ JavaScript/brand.js
@@ -4,6 +4,7 @@ var favouriteBrands= prompt("what's your favourite Brands?");
 
 if(favouriteBrands=='adidas'){
     alert ('Welcome to your small home');
+    document.write('<h3> Welcome to your small home <h3>') ;
 }else if(favouriteBrands=='Adidas'){
     alert ('Welcome to your small home');
 }else{
