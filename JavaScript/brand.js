592d1845c9995fbd6f137ba748c24199c7dbf438
Alaa-AbuIssa <alaaabuissa95@gmail.com>
2021-03-21 14:44:01 +0200

    final.

--- JavaScript/brand.js
+++ JavaScript/brand.js
@@ -1,13 +1,43 @@
 alert ("Hello There");
 
-var favouriteBrands= prompt("what's your favourite Brands?");
+var favouriteBrands = prompt("what's your favourite Brands?");
 
-if(favouriteBrands=='adidas'){
+
+while (favouriteBrands !== 'adidas' && favouriteBrands !== 'puma') {
+   favouriteBrands = prompt('please only write adidas OR puma');
+    
+}
+
+if(favouriteBrands=='adidas'||favouriteBrands=='puma' )
+{
     alert ('Welcome to your small home');
-    document.write('<h3> Welcome to your small home <h3>') ;
-}else if(favouriteBrands=='Adidas'){
-    alert ('Welcome to your small home');
-}else{
+    //document.write('<h3> Welcome to your small Home <h3>') ;
+}
+
+else{
     alert('See You in there WebPage');
 }
 
+
+var NumberOfProducts = prompt('How many Products do you want?');
+
+var Products = '';
+var result = '';
+
+if (favouriteBrands == 'puma') {
+    Products = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsZ2UshyejhPa_d8Z3JfjhGw8MQncFBf_5Rw&usqp=CAU"/> ';
+
+} else if (favouriteBrands == 'adidas') {
+    Products = '<img src="https://logos-download.com/wp-content/uploads/2016/03/Adidas_logo.png"/> ';
+    
+}
+
+console.log(Products);
+
+for (var i = 0; i < NumberOfProducts; i++) {
+    result += Products;
+}
+
+console.log(result);
+
+document.write(result);
